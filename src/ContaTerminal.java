50847c100a080088c6ecc5717cf23055499354ac
Emilly <emilly_chaves@hotmail.com>
2023-08-23 23:21:05 -0300

    Segundo commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,10 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+//ToDo: Conhecer e importar a classe Scanner
+// Exibir as mensagens para o nosso usuário
+//Obter pela classe scanner os valores digitador no terminal
+//Exibir a mensagem conta criada
+
     }
 }
