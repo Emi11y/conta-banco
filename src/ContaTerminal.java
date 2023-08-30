6f4ab83caf9d4cdb044f1ec83b2b593a37102f67
Emilly <emilly_chaves@hotmail.com>
2023-08-29 23:15:43 -0300

    atualizaçao com o codigo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-//ToDo: Conhecer e importar a classe Scanner
-// Exibir as mensagens para o nosso usuário
-//Obter pela classe scanner os valores digitador no terminal
-//Exibir a mensagem conta criada
+      int conta ;
+      String agencia ;
+      String nome ;
+      double saldo ;
+      Scanner sc1 = new Scanner(System.in);
+      Scanner sc2 = new Scanner(System.in);
+      Scanner sc3 = new Scanner(System.in);
+      Scanner sc4 = new Scanner(System.in);
+ 
+     System.out.println("Digite o número da conta: ");
+     conta = sc1.nextInt();
+      
+     System.out.println("Digite o número da agencia: ");
+     agencia = sc2.nextLine();
+     
+     System.out.println("Digite o saldo: ");
+     saldo = sc3.nextDouble();
+      
+     System.out.println("Digite o nome: ");
+     nome = sc4.nextLine();
+     
+     System.out.println("Conta criada!!");
+     System.out.println("Conta: "+conta);
+     System.out.println("Agencia: "+agencia);
+     System.out.println("Nome: "+nome);
+     System.out.println("Saldo da conta: "+saldo);
 
     }
 }
